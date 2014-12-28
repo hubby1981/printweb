50848defbd8fdc09c3483f2f91c44e0a31cdabac
marcel@bw <marcel.weissgerber@live.de>
2014-12-28 22:53:32 +0100

    Added Interface for Product

---
+++ PrintWeb/Interface/IProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintWeb.Interface
+{
+    interface IProduct
+    {
+        float Price { get; set; }
+
+        float Discount { get; set; }
+
+        IEnumerable<IProduct> Products { get; set; }
+
+
+    }
+}
